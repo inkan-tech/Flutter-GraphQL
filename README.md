0f240887736981df29c685ae8369347c74a3d40c
thomnico <thomnico@gmail.com>
2021-12-29 17:04:11 +0100

    Allow to see GraphQL with public access or GraphQL with authenticated only access. Must add the data externaly, set ACL on one of the data

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # back4appgraphqldemo
 
-
 Flutter-Back4app with caching Connection Template using GraphQL 5+
 
 ## Getting Started
@@ -11,4 +10,6 @@ You must modify lib/constants.dart with your Back4app credentials.
 
 ## Create the database
 
-I simply follow https://www.back4app.com/docs/parse-graphql/graphql-mutation-create-object#mutation-generic 
+I follow <https://www.back4app.com/docs/parse-graphql/graphql-mutation-create-object#mutation-generic>
+
+Add an ACL as authenticated to one of the row in the database.
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'constants.dart';
 
+// TODO: see https://blog.waldo.io/graphql-in-flutter/ for runMutation example
 void main() async {
   // We're using HiveStore for persistence,
   // so we need to initialize Hive.
@@ -13,14 +13,18 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final HttpLink httpLink =
-        HttpLink(kParseApiUrl, 
-        defaultHeaders: {
+    final HttpLink urlLink = HttpLink(kParseApiUrl, defaultHeaders: {
       'X-Parse-Application-Id': kParseApplicationId,
-      'X-Parse-Client-Key': kParseClientKey
+      'X-Parse-Client-Key': kParseClientKey,
+//      'X-Parse-Session-Token': 'r:8e26ccce46ec4650cc79e2b969c50674'
     });
+    final authLink = AuthLink(
+      // ignore: undefined_identifier
+      getToken: () async => 'Bearer r:8e26ccce46ec4650cc79e2b969c50674',
+    );
 
-      // 'X-Parse-REST-API-Key': kParseRestApiKey
+    var httpLink = authLink.concat(urlLink);
+
     ValueNotifier<GraphQLClient> client = ValueNotifier(
       GraphQLClient(
         cache: GraphQLCache(store: HiveStore()),
@@ -47,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String saveFormat;
   String objectId;
 
-  String query = '''
+  static String query = '''
   query FindHero {
     heroes{
       count,
@@ -61,48 +65,92 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   ''';
 
+  ///THIS IS A SAMPLE FOR MAKING MUTABLE REQUEST
+
+  static String loginQuery = '''
+    mutation LogIn{
+      logIn(input: {
+        username: "test",
+        password: "testtest"
+      }){
+        viewer{
+          sessionToken
+          user { 
+           objectId
+           id
+           emailVerified
+          }
+        }
+      }
+    }
+    ''';
+
+/////// check https://blog.logrocket.com/using-graphql-with-flutter-a-tutorial-with-examples/
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Parsing data using GraphQL',
+          appBar: AppBar(
+            title: Text(
+              'Parsing data using GraphQL',
+            ),
           ),
-        ),
-        body: Query(
-          options: QueryOptions(
-            document: gql(query),
+          body: Query(
+            options: QueryOptions(
+              document: gql(query),
+            ),
+            builder: (
+              QueryResult result, {
+              Refetch refetch,
+              FetchMore fetchMore,
+            }) {
+              print(result.exception);
+              if (result.data == null) {
+                return Center(
+                    child: Text(
+                  "Loading...",
+                  style: TextStyle(fontSize: 20.0),
+                ));
+              } else {
+                return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(result.data["heroes"]["edges"][index]["node"]
+                          ['name']),
+                      subtitle: Text("height : " +
+                          result.data["heroes"]["edges"][index]["node"]
+                                  ['height']
+                              .toString()),
+                    );
+                  },
+                  itemCount: result.data["heroes"]["edges"].length,
+                );
+              }
+            },
           ),
-          builder: (
-            QueryResult result, {
-            Refetch refetch,
-            FetchMore fetchMore,
-          }) {
-            print(result.exception);
-            if (result.data == null) {
-              return Center(
-                  child: Text(
-                "Loading...",
-                style: TextStyle(fontSize: 20.0),
-              ));
-            } else {
-              return ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(result.data["heroes"]["edges"]
-                        [index]["node"]['name']),
-                    subtitle: Text("height : "+result.data["heroes"]["edges"]
-                        [index]["node"]['height'].toString())
-                        ,
-                  );
-                },
-                itemCount: result.data["heroes"]["edges"].length,
-              );
-            }
-          },
-        ),
-      ),
+          floatingActionButton: Mutation(
+              options: MutationOptions(
+                  document: gql(loginQuery),
+                  update: (GraphQLDataProxy cache, QueryResult result) {
+                    return cache;
+                  },
+                  onCompleted: (dynamic resultData) {
+                    String _token =
+                        resultData['logIn']['viewer']['sessionToken'];
+                    print("TOKEN:" + _token);
+                    // TODO implement this: https://github.com/zino-hofmann/graphql-flutter/issues/363
+                  }),
+              builder: (
+                RunMutation runMutation,
+                QueryResult result,
+              ) {
+                return FloatingActionButton(
+                  onPressed: () =>
+                      runMutation({"username": "test", "password": "testtest"}),
+                  tooltip: 'Login',
+                  child: const Icon(Icons.login_sharp),
+                );
+              })),
     );
   }
 }
